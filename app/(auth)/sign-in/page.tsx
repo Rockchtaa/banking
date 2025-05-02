f0c4ce5c394e6f3be82f4a0a46b85bb6fa9383bd
Yassine <yassineelbrahmi11@gmail.com>
2025-05-02 16:05:34 +0200

    Refactor SignIn page for consistency and formatting improvements

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,13 +1,14 @@
-import AuthForm from '@/components/AuthForm'
-import React from 'react'
+"use client";
+
+import AuthForm from '@/components/AuthForm';
+import React from 'react';
 
 const SignIn = () => {
   return (
     <section className='flex-center size-full max-sm:px-6'>
       <AuthForm type="sign-in" />
-      
     </section>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
