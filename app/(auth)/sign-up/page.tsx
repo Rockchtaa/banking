f9aa7c78f42447993f5b5da367713a8cea2297a6
Yassine <yassineelbrahmi11@gmail.com>
2025-05-02 16:05:38 +0200

    Fix formatting and add missing newline in SignUp page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,13 +1,14 @@
-import AuthForm from '@/components/AuthForm'
-import React from 'react'
+"use client";
+
+import AuthForm from '@/components/AuthForm';
+import React from 'react';
 
 const SignUp = () => {
   return (
     <section className='flex-center size-full max-sm:px-6'>
       <AuthForm type="sign-up" />
-      
     </section>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
