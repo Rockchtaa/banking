9caf8ecd9437f6ccdbe38949a5e0d822550f8f83
Yassine <yassineelbrahmi11@gmail.com>
2025-05-03 21:17:39 +0200

    Add user actions for signIn and signUp just temporary to avoid errors

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,21 @@
+'use server';
+
+export const signIn = async ( {email, password}: signInProps) =>{
+    try {
+        //mutation / Database / fetch
+        
+    } catch (error) {
+        console.error("Error signing in:", error);
+        
+    }
+}
+
+export const signUp = async (userData : SignUpParams) =>{
+    try {
+        //mutation / Database / fetch
+        
+    } catch (error) {
+        console.error("Error signing up:", error);
+        
+    }
+}
