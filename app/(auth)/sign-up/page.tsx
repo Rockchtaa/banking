b970280a786b5f529a113d5252356d9c0bccfca6
Yassine <yassineelbrahmi11@gmail.com>
2025-05-11 23:08:32 +0200

    Remove commented-out code for getLoggedInUser in SignUp component

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,12 +1,9 @@
 // "use client";
 
 import AuthForm from '@/components/AuthForm';
-// import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react';
 
 const SignUp = async () => {
-  // const loggedInUser = await getLoggedInUser();
-  // console.log('Logged in user:', loggedInUser);
 
   return (
     <section className='flex-center size-full max-sm:px-6'>
