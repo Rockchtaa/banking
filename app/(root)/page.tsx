a352f7bde7dbb918f33ae85764a1a58525f4275d
Yassine <yassineelbrahmi11@gmail.com>
2025-05-17 14:02:31 +0200

    Refactor Home component to fetch and display user real accounts info and transactions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { get } from "http";
 import React from "react";
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page }}: SearchParamProps ) => {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn?.$id });
+  if(!accounts) return;
+  const accoountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accoountsData[0]?.appwriteItemId;
+  const account = await getAccount({ appwriteItemId });
+  // console.log({ accoountsData ,account });
   return (
+
     <>
       <div className="home">
         <div className="home-content">
@@ -14,13 +23,13 @@ const Home = async () => {
             <HeaderBox
               type="greeting"
               title="Welcome"
-              user={loggedIn?.name || "guest"}
+              user={loggedIn?.firstName || "guest"}
               subtext="Acces and manage your account and transactions effectively"
             />
             <TotalBalanceBox
-              accounts={[]}
-              totalBanks={1}
-              totalCurrentBalance={2047.5}
+              accounts={accoountsData}
+              totalBanks={accounts?.totalBanks}
+              totalCurrentBalance={accounts?.totalCurrentBalance}
             />
           </header>
         </div>
@@ -34,9 +43,9 @@ const Home = async () => {
       
       <RightSidebar 
         user={loggedIn}
-        transactions={[]}
-        banks={[{currentBalance: 2047.5}, {currentBalance: 1234.5}]}
-      /> 
+        transactions={account?.transactions}
+        banks={accoountsData?.slice(0, 2)}
+      />
       </div>
     </>
   );
