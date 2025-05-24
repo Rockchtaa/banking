753cef9e2f2aa5ecf94331aa048ef4bdb40d9453
Yassine <yassineelbrahmi11@gmail.com>
2025-05-24 20:21:38 +0200

    Add RecentTransactions import to page.tsx for future use

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
@@ -34,7 +35,7 @@ const Home = async ({ searchParams: { id, page }}: SearchParamProps ) => {
           </header>
         </div>
 
-         {/* <RecentTransactions 
+         {/* <RecentTransactions
           accounts={accountsData}
           transactions={account?.transactions}
           appwriteItemId={appwriteItemId}
